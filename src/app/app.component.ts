e117be2e4017472f267c454f8e2f5146fff00e2f
Vaishali Deshmukh <vaishalideshmukh53@gmail.com>
2023-03-29 13:10:52 +0530

    first-page css changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,10 @@ import { UsersService } from './users.service';
 })
 export class AppComponent implements OnInit{
   title = 'NewDemoApp';
-  users:any;
-  loginName:any;
 
   constructor(private userService:UsersService){}
 
   ngOnInit(){
-    this.userService.getUsersList().subscribe((res)=>{
-      console.log(res);
-      this.users = res;
-    })
   }
-  viewProfile(){
-    this.userService.getUserDetails(this.users.id);  }
   
 }
